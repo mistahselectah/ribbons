915ce477facf77d8d1a6528ecaa301f7ba133d49
Osharin Alexander <djslon@mail.ru>
2013-10-03 18:13:54 +0400

    Init

---
+++ js/main.js
@@ -0,0 +1,197 @@
+
+
+if ( !window.requestAnimationFrame ) {
+    window.requestAnimationFrame = ( function() {
+        return window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame;
+    } )();
+};
+
+var main=function() {
+    var CANVAS=document.getElementById("canvas");
+    CANVAS.width=window.innerWidth;
+    CANVAS.height=window.innerHeight;
+
+    var AMORTIZATION=0.95;
+    var drag=false;
+
+
+    var old_x, old_y;
+
+    var dX=0, dY=0;
+    var mouseDown=function(e) {
+        drag=true;
+        old_x=e.pageX, old_y=e.pageY;
+        e.preventDefault();
+        return false;
+    }
+
+    var mouseUp=function(e){
+        drag=false;
+    }
+
+    var mouseMove=function(e) {
+        if (!drag) return false;
+        dX=(e.pageX-old_x)*2*Math.PI/CANVAS.width,
+            dY=(e.pageY-old_y)*2*Math.PI/CANVAS.height;
+        THETA+=dX;
+        PHI+=dY;
+        old_x=e.pageX, old_y=e.pageY;
+        e.preventDefault();
+    }
+
+    CANVAS.addEventListener("mousedown", mouseDown, false);
+    CANVAS.addEventListener("mouseup", mouseUp, false);
+    CANVAS.addEventListener("mouseout", mouseUp, false);
+    CANVAS.addEventListener("mousemove", mouseMove, false);
+
+    /*========================= GET WEBGL CONTEXT ========================= */
+    try {
+        var GL = CANVAS.getContext("experimental-webgl", {antialias: true});
+    } catch (e) {
+        alert("You are not webgl compatible :(") ;
+        return false;
+    } ;
+
+    /*========================= SHADERS ========================= */
+
+
+    function getShader(gl, id) {
+        var shaderScript = document.getElementById(id);
+        if (!shaderScript) {
+            return null;
+        }
+
+        var str = "";
+        var k = shaderScript.firstChild;
+        while (k) {
+            if (k.nodeType == 3) {
+                str += k.textContent;
+            }
+            k = k.nextSibling;
+        }
+
+        var shader;
+        if (shaderScript.type == "x-shader/x-fragment") {
+            shader = gl.createShader(gl.FRAGMENT_SHADER);
+        } else if (shaderScript.type == "x-shader/x-vertex") {
+            shader = gl.createShader(gl.VERTEX_SHADER);
+        } else {
+            return null;
+        }
+
+        gl.shaderSource(shader, str);
+        gl.compileShader(shader);
+
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            alert(gl.getShaderInfoLog(shader));
+            return null;
+        }
+
+        return shader;
+    }
+
+    var shader_vertex=getShader(GL, "color-shader-vs");
+    var shader_fragment=getShader( GL, "color-shader-fs");
+
+    var SHADER_PROGRAM=GL.createProgram();
+    GL.attachShader(SHADER_PROGRAM, shader_vertex);
+    GL.attachShader(SHADER_PROGRAM, shader_fragment);
+
+    GL.linkProgram(SHADER_PROGRAM);
+
+    var _Pmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Pmatrix");
+    var _Vmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Vmatrix");
+    var _Mmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Mmatrix");
+
+    var _position = GL.getAttribLocation(SHADER_PROGRAM, "position");
+    var _color = GL.getAttribLocation(SHADER_PROGRAM, "color");
+    //var _normal = GL.getAttribLocation(SHADER_PROGRAM, "normal");
+
+    GL.enableVertexAttribArray(_position);
+    GL.enableVertexAttribArray(_color);
+    //GL.enableVertexAttribArray(_normal);
+
+    GL.useProgram(SHADER_PROGRAM);
+
+
+    /*========================= MATRIX ========================= */
+
+    var PROJMATRIX=LIBS.get_projection(40, CANVAS.width/CANVAS.height, 1, 100);
+    var MOVEMATRIX=LIBS.get_I4();
+    var VIEWMATRIX=LIBS.get_I4();
+
+    var THETA=0,
+        PHI=0;
+
+    LIBS.translateZ(VIEWMATRIX, -5);
+
+    /*========================= DRAWING ========================= */
+    GL.enable(GL.DEPTH_TEST);
+    GL.depthFunc(GL.LESS);
+    GL.clearColor(0.0, 0.0, 0.0, 0.0);
+    GL.clearDepth(1.0);
+    var time_old=0;
+    var render = function(count){
+        var scaleFactor = 1;
+        for(var i = 0; i<count;i++){
+            scaleFactor+=0.5;
+            //var model = primitives.plane(1,1,45);
+            //var model = primitives.triangle(1,1,45, 0.5);
+            var model = primitives.cylinder(scaleFactor,scaleFactor,120);
+
+            var vertexBuffer= GL.createBuffer ();
+            GL.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer);
+            GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(model.vertices), GL.STATIC_DRAW);
+            GL.vertexAttribPointer(_position, 3, GL.FLOAT, false,0,0) ;
+
+            var colorBuffer = GL.createBuffer ();
+            GL.bindBuffer(GL.ARRAY_BUFFER, colorBuffer);
+            GL.bufferData(GL.ARRAY_BUFFER,new Float32Array(model.colors),GL.STATIC_DRAW);
+            GL.vertexAttribPointer(_color, 3, GL.FLOAT, false,0,0);
+
+            /*var normalBuffer = GL.createBuffer ();
+            GL.bindBuffer(GL.ARRAY_BUFFER, normalBuffer);
+            GL.bufferData(GL.ARRAY_BUFFER,new Float32Array(model.normals),GL.STATIC_DRAW);
+            GL.vertexAttribPointer(_normal, 3, GL.FLOAT, false,0,0);*/
+
+            var facesBuffer = GL.createBuffer ();
+            GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, facesBuffer);
+            GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(model.faces), GL.STATIC_DRAW);
+
+            GL.drawElements(GL.TRIANGLES, model.faces.length, GL.UNSIGNED_SHORT, 0);
+        }
+    }
+
+    var animate=function(time) {
+        var dt=time-time_old;
+        if (!drag) {
+            dX*=AMORTIZATION, dY*=AMORTIZATION;
+            THETA+=dX, PHI+=dY;
+        }
+
+        LIBS.set_I4(MOVEMATRIX);
+        LIBS.rotateY(MOVEMATRIX, THETA);
+        LIBS.rotateX(MOVEMATRIX, PHI);
+        time_old=time;
+
+        GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);
+        GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
+        GL.enable(GL.DEPTH_TEST);
+        GL.depthFunc(GL.LESS);
+        GL.enable(GL.BLEND);
+        GL.blendFunc(GL.SRC_ALPHA, GL.ONE);
+        GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
+        GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
+        GL.uniformMatrix4fv(_Mmatrix, false, MOVEMATRIX);
+
+        render(1);
+
+        GL.flush();
+
+        window.requestAnimationFrame(animate);
+    }
+    animate(0);
+}
---
+++ js/primitives.js
@@ -0,0 +1,159 @@
+var primitives = {
+
+    getNormal: function(a,b,c){
+        var edge1 = this.subVector(a,b);
+        var edge2 = this.subVector(a,c);
+        return this.crossProduct(edge1,edge2);
+    },
+
+    subVector: function(a,b){
+        var result = [];
+        result.push(b[0]-a[0], b[1]-a[1], b[2]-a[2]);
+        return result;
+    },
+
+    crossProduct: function(a, b){
+        var result = [];
+        result[0] = a[1] * b[2] - a[2] * b[1];
+        result[1] = a[2] * b[0] - a[0] * b[2];
+        result[2] = a[0] * b[1] - a[1] * b[0];
+        return result;
+    },
+
+    getCoords: function(vertArray, index){
+        return [vertArray[index],vertArray[index+1],vertArray[index+2]];
+    },
+
+    pushNormals: function(vertices, faces, normals){
+        var count = 1;
+        var a, b, c, normal;
+        for(var j = 0; j<faces.length; j++){
+            switch(count){
+                case 1:
+                    a = this.getCoords(vertices, j);
+                    count++;
+                    break;
+                case 2:
+                    b = this.getCoords(vertices, j);
+                    count++;
+                    break
+                case 3:
+                    c = this.getCoords(vertices, j);
+                    normal = this.getNormal(a,b,c);
+                    normals.push(normal[0],normal[1],normal[2]);
+                    normals.push(normal[0],normal[1],normal[2]);
+                    normals.push(normal[0],normal[1],normal[2]);
+                    count = 1;
+                    break;
+            }
+        }
+    },
+
+    plane: function(width, length,angle){
+
+        var vertices = [
+            -0.5,-0.5,-0.5,
+            -0.5,0.5,0,
+            0.5,-0.5,0,
+            0.5,0.5,-0.5
+
+        ];
+
+        var colors = [];
+        var faces = [];
+        var normals = [];
+
+        for(var i = 0; i<vertices.length; i++){
+            colors.push(Math.abs(vertices[i]));
+        }
+
+        faces.push(0,1,2);
+        faces.push(2,1,3);
+
+        this.pushNormals(vertices, faces, normals);
+
+        return {vertices: vertices, colors: colors, faces: faces, normals: normals};
+
+    },
+
+    triangle: function(lA,lB,angle, z){
+        var x1, x2, y1, y2, a, b, c;
+        var vertices = [];
+        var colors = [];
+        var faces = [];
+        var normals = [];
+
+        vertices.push(0,0,z);
+        colors.push(0,0,0);
+
+        x1 = Math.sin(LIBS.degToRad(0))*lA;
+        y1 = Math.cos(LIBS.degToRad(0))*lA;
+        vertices.push(x1,y1,z);
+        colors.push(1,0,0);
+
+        x2 = Math.sin(LIBS.degToRad(angle))*lB;
+        y2 = Math.cos(LIBS.degToRad(angle))*lB;
+        vertices.push(x2,y2,z);
+        colors.push(0,0,1);
+
+        faces.push(0,1,2);
+
+        this.pushNormals(vertices, faces, normals);
+
+        return {vertices: vertices, colors: colors, faces: faces, normals: normals};
+    },
+
+    cone: function(radius, height, rate){
+        var vertices = [];
+        var colors = [];
+        var faces = [];
+        var normals = [];
+        vertices.push(0,0,height/2);
+        colors.push(0,0,1)
+        var z = -1*height/2;
+        for(var i = 0; i<rate; i++){
+            var angle = LIBS.degToRad(360/rate*i);
+            var x = Math.sin(angle)*radius;
+            var y =  Math.cos(angle)*radius;
+            //vertice coords
+            vertices.push(x, y, z);
+            //color for vertice
+            colors.push(0, 1,1);
+            faces.push(0, i+1,i<rate-1?i+2:1);
+        }
+
+        return {vertices: vertices, colors: colors, faces: faces, normals: normals};
+    },
+
+    cylinder: function(radius, height, rate){
+        var vertices = [];
+        var colors = [];
+        var faces = [];
+        var normals = [];
+
+        for(var i = 0; i<rate; i++){
+            var angle = LIBS.degToRad(360/rate*i);
+            var x = Math.sin(angle)*radius;
+            var y =  Math.cos(angle)*radius;
+            var z = height/2;
+            //vertice coords
+            vertices.push(x, y, z);
+            colors.push(0, 0,1);
+
+            vertices.push(x, y, -1*z);
+            colors.push(1, 0,0);
+
+            if(i<rate-1){
+                faces.push(i*2, i*2+1,i*2+2);
+                faces.push(i*2+2, i*2+1, i*2+3);
+            }else{
+                faces.push(i*2, i*2+1,0);
+                faces.push(i*2+1, 0,1);
+            }
+        }
+
+        this.pushNormals(vertices, faces, normals);
+
+        return {vertices: vertices, colors: colors, faces: faces, normals: normals};
+    }
+}
