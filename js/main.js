84121fd79c271245cacfaee23057a343f1e97b22
Osharin Alexander <djslon@mail.ru>
2013-10-04 15:07:39 +0400

    working light shader from - http://www.opengl-tutorial.org/beginners-tutorials/tutorial-8-basic-shading/

--- js/main.js
+++ js/main.js
@@ -93,8 +93,8 @@ var main=function() {
         return shader;
     }
 
-    var shader_vertex=getShader(GL, "shader-vs");
-    var shader_fragment=getShader( GL, "shader-fs");
+    var shader_vertex=getShader(GL, "light-shader-vs");
+    var shader_fragment=getShader( GL, "light-shader-fs");
 
     var SHADER_PROGRAM=GL.createProgram();
     GL.attachShader(SHADER_PROGRAM, shader_vertex);
@@ -105,6 +105,7 @@ var main=function() {
     var _Pmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Pmatrix");
     var _Vmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Vmatrix");
     var _Mmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Mmatrix");
+    var _wsLightPosition = GL.getUniformLocation(SHADER_PROGRAM, "wsLightPosition");
 
     var _position = GL.getAttribLocation(SHADER_PROGRAM, "position");
     var _color = GL.getAttribLocation(SHADER_PROGRAM, "color");
@@ -126,7 +127,7 @@ var main=function() {
     var THETA=0,
         PHI=0;
 
-    LIBS.translateZ(VIEWMATRIX, -25);
+    LIBS.translateZ(VIEWMATRIX, -5);
 
     /*========================= DRAWING ========================= */
     //GL.enable(GL.DEPTH_TEST);
@@ -142,8 +143,8 @@ var main=function() {
             scaleFactor+=0.5;
             //var model = primitives.plane(1,1,45);
             //var model = primitives.triangle(1,1,45, 0.5);
-            //var model = primitives.cylinder(scaleFactor,scaleFactor,36);
-            var model = primitives.cone(scaleFactor,scaleFactor,36);
+            var model = primitives.cylinder(scaleFactor,scaleFactor,120);
+            //var model = primitives.cone(scaleFactor,scaleFactor,36);
 
             var vertexBuffer= GL.createBuffer ();
             GL.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer);
@@ -159,6 +160,7 @@ var main=function() {
             GL.bindBuffer(GL.ARRAY_BUFFER, normalBuffer);
             GL.bufferData(GL.ARRAY_BUFFER,new Float32Array(model.normals),GL.STATIC_DRAW);
             GL.vertexAttribPointer(_normal, 3, GL.FLOAT, false,0,0);
+
 
             var facesBuffer = GL.createBuffer ();
             GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, facesBuffer);
@@ -185,8 +187,9 @@ var main=function() {
         GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
         GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
         GL.uniformMatrix4fv(_Mmatrix, false, MOVEMATRIX);
+        GL.uniform3f(_wsLightPosition, 4,4,4);
 
-        render(10);
+        render(3);
 
         GL.flush();
 
